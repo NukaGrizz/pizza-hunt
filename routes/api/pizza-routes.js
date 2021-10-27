7cb109f2b38da29bd5595f0949e8d0631ec18e09
NukaGrizz <nukagrizz@gmail.com>
2021-10-26 20:10:46 -0400

    update

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -1,20 +1,19 @@
 const router = require('express').Router();
-
 const {
-    getAllPizza,
-    getPizzaById,
-    createPizza,
-    updatePizza,
-    deletePizza
+  getAllPizza,
+  getPizzaById,
+  createPizza,
+  updatePizza,
+  deletePizza
 } = require('../../controllers/pizza-controller');
 
-// Set up Get all and Post at /api/pizzas
+// /api/pizzas
 router
   .route('/')
   .get(getAllPizza)
   .post(createPizza);
 
-// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+// /api/pizzas/:id
 router
   .route('/:id')
   .get(getPizzaById)
